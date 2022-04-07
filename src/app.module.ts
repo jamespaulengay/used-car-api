5badda61bb8feaf83035650a2265ef93df8b94d4
James Paul Engay <jamespaul.engay@tup.edu.ph>
2022-04-07 18:13:53 +0800

    Persist Data with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // Type of Database
+      database: 'db.sqlite', // Filename
+      entities: [User, Report], // Loaded entities
+      synchronize: true, // Update structure of the database automatically //DEVELOPMENT
+    }),
+    ReportsModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/users/user.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+}
